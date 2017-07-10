769cdfb04a22f6581993fb552f5c46a5b70fcd2b
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-07-10 11:20:28 -0500

    initial commit -- assets from whiplash-child-app

---
+++ javascripts/behavior.js
@@ -0,0 +1,31 @@
+document.addEventListener('turbolinks:load', handleUIBehavior);
+
+function handleUIBehavior() {
+  var mobileNavButton = document.querySelector('.mobile-nav-button');
+  var subMenuItem = document.querySelectorAll('.sub-menu > a');
+
+  mobileNavButton.addEventListener('click', mobileNavClickHandler);
+
+  Array.prototype.forEach.call(subMenuItem, function (menu) {
+    menu.addEventListener('click', subMenuClickHandler);
+  });
+}
+
+function mobileNavClickHandler() {
+  var button = document.querySelector('.mobile-nav-button');
+  var sideMenu = button.nextElementSibling.classList.contains('menu-wrapper');
+  var menu = sideMenu ? button.nextElementSibling : document.querySelector('.top-menu .menu');
+
+  button.classList.toggle('open');
+  menu.classList.toggle('open');
+}
+
+function subMenuClickHandler(event) {
+  var subMenuList = event.target.nextElementSibling;
+
+  if (!subMenuList.classList.contains('active')) {
+    event.preventDefault();
+  }
+
+  subMenuList.classList.toggle('active');
+}
