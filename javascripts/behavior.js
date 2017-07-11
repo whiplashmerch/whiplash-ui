6228bb039ee012160ece084349b81281d839d01c
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-07-11 01:14:42 -0500

    add DotLoader loading indicator clone

--- javascripts/behavior.js
+++ javascripts/behavior.js
@@ -3,11 +3,20 @@ document.addEventListener('turbolinks:load', handleUIBehavior);
 function handleUIBehavior() {
   var mobileNavButton = document.querySelector('.mobile-nav-button');
   var subMenuItem = document.querySelectorAll('.sub-menu > a');
+  var loaders = document.querySelectorAll('.loader-wrapper');
 
   mobileNavButton.addEventListener('click', mobileNavClickHandler);
 
   Array.prototype.forEach.call(subMenuItem, function (menu) {
     menu.addEventListener('click', subMenuClickHandler);
+  });
+
+  Array.prototype.forEach.call(loaders, function (loader) {
+    for (var dot, i = 0; i < 3; i++) {
+      dot = document.createElement('div');
+      dot.classList.add('loader-dot');
+      loader.appendChild(dot);
+    }
   });
 }
 
